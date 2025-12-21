ffd0ea7184348a37e02d6b0257230e00c200dc5a
CanC-Code <cardscollectiblesandoddities@gmail.com>
2025-12-21 02:02:28 -0700

    Update project_graph.js

--- core/project_graph.js
+++ core/project_graph.js
@@ -5,10 +5,11 @@
 
 class ProjectGraph {
     constructor() {
-        this.scenes = {};   // scene_name -> scene_data
-        this.assets = {};   // asset_path -> asset_data
+        this.scenes = {};   // sceneName -> sceneData
+        this.assets = {};   // assetPath -> { data, type }
+
         this.templates = {
-            Snake: null, // can later load templates if desired
+            Snake: null,
             TicTacToe: null,
             RPG: null
         };
@@ -20,31 +21,35 @@ class ProjectGraph {
         this.onAssetAdded = null;
         this.onProjectReadyForExport = null;
 
-        console.log("ProjectGraph initialized with templates:", Object.keys(this.templates));
+        console.log(
+            "ProjectGraph initialized. Templates:",
+            Object.keys(this.templates)
+        );
     }
 
     // ------------------------------
     // Scene Management
     // ------------------------------
     addScene(sceneName) {
-        if (this.scenes[sceneName]) {
-            console.warn(`Scene '${sceneName}' already exists.`);
+        if (!sceneName || this.scenes[sceneName]) {
+            console.warn(`Scene '${sceneName}' already exists or invalid.`);
             return false;
         }
+
         this.scenes[sceneName] = {
+            root_node_type: "Node2D",
             nodes: {},
-            scripts: {},
-            root_node_type: "Node2D"
+            scripts: {}
         };
-        if (typeof this.onSceneAdded === "function") this.onSceneAdded(sceneName);
+
+        if (typeof this.onSceneAdded === "function") {
+            this.onSceneAdded(sceneName);
+        }
         return true;
     }
 
     removeScene(sceneName) {
-        if (!this.scenes[sceneName]) {
-            console.warn(`Scene '${sceneName}' does not exist.`);
-            return false;
-        }
+        if (!this.scenes[sceneName]) return false;
         delete this.scenes[sceneName];
         return true;
     }
@@ -53,23 +58,35 @@ class ProjectGraph {
         return this.scenes[sceneName] || null;
     }
 
+    getScenes() {
+        return this.scenes;
+    }
+
     // ------------------------------
     // Node Management
     // ------------------------------
-    addNode(sceneName, nodeName, nodeType="Node2D", parentName="") {
+    addNode(sceneName, nodeName, nodeType = "Node2D", parentName = "") {
         const scene = this.getScene(sceneName);
         if (!scene) return false;
+
         if (scene.nodes[nodeName]) {
-            console.warn(`Node '${nodeName}' already exists in scene '${sceneName}'.`);
+            console.warn(
+                `Node '${nodeName}' already exists in scene '${sceneName}'.`
+            );
             return false;
         }
+
         scene.nodes[nodeName] = {
             type: nodeType,
             parent: parentName,
             properties: {},
-            scripts: []
+            scripts: [] // script filenames
         };
-        if (typeof this.onNodeAdded === "function") this.onNodeAdded(sceneName, nodeName);
+
+        if (typeof this.onNodeAdded === "function") {
+            this.onNodeAdded(sceneName, nodeName);
+        }
+
         return true;
     }
 
@@ -83,30 +100,51 @@ class ProjectGraph {
     setNodeProperty(sceneName, nodeName, propertyName, value) {
         const scene = this.getScene(sceneName);
         if (!scene || !scene.nodes[nodeName]) return false;
+
         scene.nodes[nodeName].properties[propertyName] = value;
         return true;
     }
 
-    attachScriptToNode(sceneName, nodeName, scriptName) {
+    attachScriptToNode(sceneName, nodeName, scriptFileName) {
         const scene = this.getScene(sceneName);
         if (!scene || !scene.nodes[nodeName]) return false;
-        scene.nodes[nodeName].scripts.push(scriptName);
-        if (typeof this.onScriptAdded === "function") this.onScriptAdded(sceneName, scriptName);
+
+        if (!scene.scripts[scriptFileName]) {
+            console.warn(
+                `Script '${scriptFileName}' does not exist in scene '${sceneName}'.`
+            );
+            return false;
+        }
+
+        scene.nodes[nodeName].scripts.push(scriptFileName);
+
+        if (typeof this.onScriptAdded === "function") {
+            this.onScriptAdded(sceneName, scriptFileName);
+        }
+
         return true;
     }
 
     // ------------------------------
     // Script Management
     // ------------------------------
-    addScript(sceneName, scriptName, scriptCode) {
+    addScript(sceneName, scriptName, scriptCode = "") {
         const scene = this.getScene(sceneName);
         if (!scene) return false;
+
         if (scene.scripts[scriptName]) {
-            console.warn(`Script '${scriptName}' already exists in scene '${sceneName}'.`);
+            console.warn(
+                `Script '${scriptName}' already exists in scene '${sceneName}'.`
+            );
             return false;
         }
+
         scene.scripts[scriptName] = scriptCode;
-        if (typeof this.onScriptAdded === "function") this.onScriptAdded(sceneName, scriptName);
+
+        if (typeof this.onScriptAdded === "function") {
+            this.onScriptAdded(sceneName, scriptName);
+        }
+
         return true;
     }
 
@@ -120,13 +158,21 @@ class ProjectGraph {
     // ------------------------------
     // Asset Management
     // ------------------------------
-    addAsset(assetPath, assetData) {
+    addAsset(assetPath, assetData, assetType = "binary") {
         if (this.assets[assetPath]) {
             console.warn(`Asset '${assetPath}' already exists.`);
             return false;
         }
-        this.assets[assetPath] = assetData;
-        if (typeof this.onAssetAdded === "function") this.onAssetAdded(assetPath);
+
+        this.assets[assetPath] = {
+            data: assetData,
+            type: assetType
+        };
+
+        if (typeof this.onAssetAdded === "function") {
+            this.onAssetAdded(assetPath);
+        }
+
         return true;
     }
 
@@ -144,38 +190,15 @@ class ProjectGraph {
         return this.assets;
     }
 
-    getScenes() {
-        return this.scenes;
-    }
-
     // ------------------------------
-    // Export Placeholder
+    // Export Hook
     // ------------------------------
     exportProjectZip(outputPath) {
-        console.log(`Export requested to '${outputPath}'`);
-        if (typeof this.onProjectReadyForExport === "function") this.onProjectReadyForExport(outputPath);
-        return true;
-    }
-
-    // ------------------------------
-    // Scene file generation (tscn)
-    // ------------------------------
-    generateSceneFile(sceneName) {
-        const scene = this.getScene(sceneName);
-        if (!scene) return null;
-        let sceneText = `[gd_scene load_steps=2 format=2]\n`;
-        sceneText += `[node name="${sceneName}" type="${scene.root_node_type}"]\n`;
-        for (const nodeName in scene.nodes) {
-            const node = scene.nodes[nodeName];
-            sceneText += `[node name="${nodeName}" type="${node.type}" parent="${node.parent}"]\n`;
-            for (const prop in node.properties) {
-                sceneText += `${prop} = ${JSON.stringify(node.properties[prop])}\n`;
-            }
-            for (const scriptName of node.scripts) {
-                sceneText += `script = ExtResource( ${scriptName} )\n`;
-            }
+        console.log(`Export requested → ${outputPath}`);
+        if (typeof this.onProjectReadyForExport === "function") {
+            this.onProjectReadyForExport(outputPath);
         }
-        return sceneText;
+        return true;
     }
 }
 
